e07764819196f1b45ea478da660c9448432bdcf8
DELL <DELL@DESKTOP-K5323UE>
2024-04-28 17:51:08 +0530

    First Commit

---
+++ src/test/java/com/reqers/base/BaseTest.java
@@ -0,0 +1,15 @@
+package com.reqers.base;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class BaseTest {
+
+	public static RequestSpecification httpRequest;
+
+	public static Response response;
+
+	public String empID = "22";
+	
+
+}
